1e97247f7bc348747658de5b91317fd46bf8296d
DhruvR0 <143010193+DhruvR0@users.noreply.github.com>
2024-07-24 22:17:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import Mains from './Components/Mains/Mains'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <Mains /> 
+        <Footer />
+       </div>
+    </>
+  )
+}
+
+export default App
